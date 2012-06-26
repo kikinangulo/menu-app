69f9f9b2e2f3eaddd1813cbce2289305752af616
Jordi Nebot <llengot@gmail.com>
2012-06-26 12:55:32 +0200

    Testing replacement PHP->JS SDK

--- index.php
+++ index.php
@@ -19,19 +19,19 @@
 	
 	if ($user) {
 	  try {
-		// Proceed knowing you have a logged in user who's authenticated.
-		$user_profile = $facebook->api('/me');
-	  } catch (FacebookApiException $e) {
-		error_log($e);
-		$user = null;
-	  }
+			// Proceed knowing you have a logged in user who's authenticated.
+			$user_profile = $facebook->api('/me');
+		} catch (FacebookApiException $e) {
+			error_log($e);
+			$user = null;
+		}
 	}
 	
 	// Login or logout url will be needed depending on current user state.
 	if ($user) {
-	  $logoutUrl = $facebook->getLogoutUrl();
+	  	$logoutUrl = $facebook->getLogoutUrl(array('next' => 'http://dod-restaurant.cat/menu'));
 	} else {
-	  $loginUrl = $facebook->getLoginUrl();
+	  	$loginUrl = $facebook->getLoginUrl();
 	}
 	
 ?>
@@ -42,9 +42,49 @@
 <head>
 <meta charset="UTF-8">
 <title>Menú diari</title>
+<script src="http://connect.facebook.net/en_US/all.js"></script>
 </head>
 
 <body>
-	<?php print_r($user_profile); ?>
+
+
+    <div id="fb-root"></div>
+    <script>
+	
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '461415507219301', // App ID
+          status     : true, // check login status
+          cookie     : true, // enable cookies to allow the server to access the session
+          xfbml      : true  // parse XFBML
+        });
+    
+        // Additional initialization code here
+      };
+    
+      // Load the SDK Asynchronously
+      (function(d){
+         var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement('script'); js.id = id; js.async = true;
+         js.src = "//connect.facebook.net/en_US/all.js";
+         ref.parentNode.insertBefore(js, ref);
+       }(document));
+	   
+	   
+    </script>
+    
+    <fb:login-button autologoutlink="true" onlogin="OnRequestPermission();">
+    </fb:login-button>
+    <script language="javascript" type="text/javascript">
+        FB.init({
+            appId: 'Your_Application_ID',
+            status: true,
+            cookie: true,
+            xfbml: true
+        });
+    </script>
+
+
 </body>
 </html>
