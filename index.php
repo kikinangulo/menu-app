e0be794553caae83d00627fff2c564cf4042b4d4
Jordi Nebot <llengot@gmail.com>
2012-06-26 12:02:37 +0200

    Added Facebook authentication

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
+<?php 
+
+	require 'fb-sdk/facebook.php';
+	
+	// Create our Application instance (replace this with your appId and secret).
+	$facebook = new Facebook(array(
+	  'appId'  => '461415507219301',
+	  'secret' => '0b59ed580329ff317cd100e01eda0bd6',
+	));
+	
+	// Get User ID
+	$user = $facebook->getUser();
+	
+	// We may or may not have this data based on whether the user is logged in.
+	//
+	// If we have a $user id here, it means we know the user is logged into
+	// Facebook, but we don't know if the access token is valid. An access
+	// token is invalid if the user logged out of Facebook.
+	
+	if ($user) {
+	  try {
+		// Proceed knowing you have a logged in user who's authenticated.
+		$user_profile = $facebook->api('/me');
+	  } catch (FacebookApiException $e) {
+		error_log($e);
+		$user = null;
+	  }
+	}
+	
+	// Login or logout url will be needed depending on current user state.
+	if ($user) {
+	  $logoutUrl = $facebook->getLogoutUrl();
+	} else {
+	  $loginUrl = $facebook->getLoginUrl();
+	}
+	
+?>
+
+
 <!doctype html>
 <html>
 <head>
@@ -6,5 +45,6 @@
 </head>
 
 <body>
+	<?php print_r($user_profile); ?>
 </body>
 </html>
